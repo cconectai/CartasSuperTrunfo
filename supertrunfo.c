2c2bcbeaf660698d1b0dc63d2ef964f6cb05c477
cconectai <cconectai100@outlook.com>
2025-04-23 13:31:54 -0300

    Create supertrunfo.c

---
+++ supertrunfo.c
@@ -0,0 +1,30 @@
+#iclude <stdio.h>
+int manin(){
+
+char carta1, carta2;
+int Populacao1, Populacao2;
+float Area1, Area2;
+float PIB1, PIB2;
+int Pontos_turisticos1, Pontos_turisticos2;
+
+printf("Digite sua cidade: \n");
+scanf("%d", &carta1, &carta2);
+
+printf("Digite sua populacao: \n");
+scanf("%i", &Populacao1, &Populacao2);
+
+printf("Digite sua area: \n");
+scanf("%f", &Area1, &Area2);
+
+printf("Digite seu PIB: \n");
+scanf("%f", &PIB1, PIB2);
+
+printf("Digite pontos_turistico: \n");
+scanf("%d", &Pontos_turisticos1, Pontos_turisticos2);
+
+return 0;
+
+
+}
+
+
